6cba10c18f925de13af4d3e33180827bba265773
Thet Win Aung <minthit21dj@gmail.com>
2019-01-12 16:46:51 +0900

    Add github api sample

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 import BaseRouter from './BaseRouter'
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('https://api.github.com/repos/sinpaout/first-github/issues', {
+      params: {
+        state: 'all'
+      }
+    })
+    .then(function (response) {
+      const issues = response.data.filter(issue => !issue.pull_request)
+      console.log('response', issues);
+    })
+  }
+
   render() {
     return (
       <div className="App">
