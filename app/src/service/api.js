3f1e300fa4611f83b643d58c312634caf9fb988e
Thet Win Aung <minthit21dj@gmail.com>
2019-03-03 14:25:38 +0900

    Replace token

--- app/src/service/api.js
+++ app/src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'https://api.github.com/',
   timeout: 3000,
-  headers: {'Authorization': 'token b50b42dc2ca7fea1c34169a805891711c0753571'}
+  headers: {'Authorization': `token ${process.env.REACT_APP_GITHUB_TOKEN}`}
 })
 
 export default api
